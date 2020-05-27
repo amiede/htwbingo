792ecb62289d545b6285825a93ba9528c1fc7974
André Miede <amiede@users.noreply.github.com>
2020-05-27 18:35:03 +0200

    Erste Adaption

---
+++ js/data.js
@@ -0,0 +1,38 @@
+
+var headerText = "htw-Video-Konferenzen-Bingo™";
+
+var footerText = "Viel Spaß! <a href=\"https://github.com/amiede/htwbingo/\">Quellcode bei GitHub verfügbar.</a>";
+
+var winText = "Gewonnen! Seite neu laden für ein neues Spiel.";
+
+var clickSnd = new Audio("audio/click.mp3");
+
+var winSnd = new Audio("audio/win.mp3");
+
+var JSONBingo = {"squares": [
+        {"square": "\"Könnt Ihr mich hören?\""},
+        {"square": "\"Könnt Ihr mich sehen?\""},
+        {"square": "\"Kann man das geteilte Fenster sehen?\""},
+        {"square": "\"Freiheit von Forschung und Lehre!\""},
+        {"square": "\"Die Studierenden beteiligen sich nicht.\""},
+        {"square": "Viele reden durcheinander"},
+        {"square": "\"Ist noch jemand da?\""},
+        {"square": "\"Wer fehlt denn noch?\""},
+        {"square": "Kind wird beim Namen gerufen"},
+        {"square": "Kind läuft durch das Bild"},
+        {"square": "\"Es gibt eine Rückkopplung.\""},
+        {"square": "Stille"},
+        {"square": "\"Mein Mikro war stumm geschaltet.\""},
+        {"square": "\"Sprechen Sie mit uns?\""},
+        {"square": "Geschrei im Hintergrund"},
+        {"square": "Hund bellt"},
+        {"square": "\"Können Sie bitte Ihr Mikro stumm schalten?\""},
+        {"square": "\"Upp, ich bin eben rausgeflogen.\""},
+        {"square": "\"Mein Netz ist schwach.\""},
+        {"square": "\"Warten wir noch kurz, vielleicht kommt noch jemand.\""},
+        {"square": "BENUTZER hat den Anruf verlassen"},
+        {"square": "\"MS Teams ist super.\""},
+        {"square": "\"Big Blue Button ist super.\""},
+        {"square": "\"Moodle ist super.\""}   	
+    ]
+};
